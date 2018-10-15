5fb2d72ff2d5057dc749af90eb207b49dc0d6319
prashant.gangwar <prashant.gangwar@sofocle.com>
2018-10-15 18:12:46 +0530

    update code

---
+++ ico-lib.sol
@@ -0,0 +1,134 @@
+
+pragma solidity ^0.4.24;
+import "./token.sol";
+contract ico is Ownable {
+    uint  decimal = 18; //decimal of 18th for one unit of crncy
+    uint public valueOfEther = 1500; // 1 sofoCoin = 1 ehter 
+    uint public minTradeAmt =100000000000000000;//0.1eth
+    uint public hardCap = 1000 * (10 ** 18);//1000 ether
+    uint public softCap =  500 * (10 ** 18);//500 ether
+    uint public weiRaised;
+    uint public coinsDistrubuted; //coins distrubuted or amt of coins bought by users
+    uint public startDate ;
+    uint public endDate ;
+    bool public stateActive;
+    bool public statePending = true;
+    bool public icoIsSuccessfull;
+    uint public maxTokenCanBuy = 150000 * (10 ** decimal);//to achieve decentralisation 100 ether capping
+    // uint public contributionPercentage ;
+    //mapping (address => uint)  discountMapping;
+    mapping (address => uint)  weiPaidMapping;
+/* 
+    struct state public{
+    bool public stateActive;
+    bool public statePending;
+    bool public icoIsSuccessfull;
+    } */
+    
+     /*constructor updated  var*/
+   // address tokenContractOwner ; // adress of token contract owner
+    uint public  totalSupply ; //totalSupply of this ico defined within token contract
+    sofoCoin ercObject; // created object of token contract 
+
+    event TokenBuyer(address indexed buyer , uint weiPaid);
+    event EtherWithdrawn(address indexed buyer ,uint ethers);
+    
+  constructor (address tokenaddress) public{
+     ercObject = sofoCoin(tokenaddress);
+     totalSupply = ercObject.totalSupply();
+   //  tokenContractOwner = ercObject.getTokenOwner();
+     startDate = now;
+     endDate = now + 2592000;// 28 + 2 days ico
+  } 
+  
+   // modifier afterDeadline() { if (now >= endDate) _; }
+
+ /*   function calcuateRate (uint amount) internal view returns(uint token){
+     amount = amount * (valueOfEther) * (10 ** decimal); // return token = wei sent * 1
+     amount = amount /(10 ** 18);
+     return amount;
+    }*/
+    
+    function buyToken() public payable { //
+      require ((weiRaised +msg.value <= hardCap) && (endDate>=now) && (msg.value >=minTradeAmt) && (startDate<=now));
+      //uint sofotoken = calcuateRate(msg.value);//here tokens are the mini. value of sofocoin sofo
+      uint bonusSofotoken  = calDiscount(msg.value);
+      require(bonusSofotoken <= ercObject.balanceOf(address(this)));//CHECK bal of this contract in terms of sofotoken
+      require(ercObject.balanceOf(msg.sender) +bonusSofotoken <= maxTokenCanBuy );
+      ercObject.transfer(msg.sender, bonusSofotoken);// transfer function caall now msg.sender is icoContract
+      coinsDistrubuted= coinsDistrubuted.add(bonusSofotoken);
+      weiRaised = msg.value.add(weiRaised);
+    //   discountMapping[msg.sender]= bonusSofotoken - sofotoken;//discount given or extra tokens given
+      weiPaidMapping[msg.sender] = weiPaidMapping[msg.sender].add(msg.value); // wei paid  by user to return back the exact amt given by the user(buyer)
+      emit TokenBuyer(msg.sender , msg.value);
+    }
+
+    function calDiscount (uint amount) view public returns(uint bonus)  {
+        
+      amount = amount * (valueOfEther) * (10 ** decimal); // return token = wei sent * 1
+      amount = amount.div(10 ** 18);
+        
+      if((coinsDistrubuted < 1000000 * (10 ** decimal))  && (now <= startDate + 604800)){ //10 ^20 , 604800=1 week
+        bonus = 40;         
+      }
+      else if((coinsDistrubuted < 2000000 * (10 ** decimal))  && (now <= startDate + 604800 *2 )){ //week 2
+        bonus = 30;
+      }
+      else if((coinsDistrubuted < 3000000 * (10 ** decimal))  && (now <= startDate + 604800 *3)){ // week 3 
+        bonus = 20;
+      }
+      else if((coinsDistrubuted < 4000000 * (10 ** decimal))  && (now <= startDate + 604800 *4)){ // week 4 
+        bonus = 10;              
+      }
+      else{
+        bonus = 0;
+      }
+      amount = amount + (amount/100)*bonus;
+      return amount;
+    }
+
+       /* This unnamed function is called whenever someone tries to send ether to it */
+    function  () public payable{
+      buyToken();
+    }
+  
+  function getBackEther() public returns(bool sucess){
+      require((endDate < now) && (weiRaised < softCap) && (weiPaidMapping[msg.sender] > 0));// can execute function only after end date && 499 or less < 500(softCap)
+      emit EtherWithdrawn(msg.sender,weiPaidMapping[msg.sender]);
+      uint etherReturn = weiPaidMapping[msg.sender];    
+      weiPaidMapping[msg.sender] = 0;
+      require (msg.sender.send(etherReturn));//sending ether to the users account //require for recurrsion attack
+      return true;
+
+  }
+  
+  function withdrawEther() public onlyOwner {
+      require (weiRaised >= softCap);// no one can buy token after end date of ico so ico owner can not transact ethers to his account
+      msg.sender.transfer(address(this).balance);
+
+  }
+
+  function icoState () public {
+    if(startDate < now){
+      statePending = false;
+    }
+    if(endDate > now && weiRaised<hardCap){
+      stateActive = true;
+    }
+    else{
+        stateActive = false;
+    }
+
+    if(weiRaised > softCap)
+    {
+      icoIsSuccessfull = true;
+    }
+
+  }
+  
+  function contributionPercent() public view returns(uint percentage){
+      // let buyer know what percentage they hold in ico
+    return  (ercObject.balanceOf(msg.sender).mul(100)).div(totalSupply) * (10 ** decimal);
+  }
+  
+}
---
+++ token-lib.sol
@@ -0,0 +1,145 @@
+pragma solidity ^0.4.24;
+
+/*import "./Ownable.sol";
+*/
+
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
+ contract ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    event Burn(address indexed tokenOwner,uint tokens);
+}
+contract Ownable {
+    address public owner;
+    using SafeMath for uint256;
+
+    constructor () public {
+        owner = msg.sender; 
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+     function transferOwnership(address newOwner) public onlyOwner {
+        if (newOwner != address(0)) {
+            owner = newOwner;
+        }
+    }
+}
+
+contract sofoCoin is ERC20Interface ,Ownable{   
+ string constant tokenName = "SofoCoin"; //
+ string constant symbol = "Sofo";
+ mapping (address => uint)  coinBalance;
+ mapping(address => mapping (address => uint256)) public allowed;
+ uint  decimal = 18; //decimal of 18th for one unit of crncy
+ uint public initialSupply ;
+ address public owner;
+ uint public  totalSupply;
+    
+    constructor() public {
+        totalSupply = 3000000000 * (10 ** decimal) ;// 300million tokens 3000000000 * 000000000000000000
+        owner = msg.sender;
+        coinBalance[owner]= totalSupply;
+    }
+    
+    function  totalSupply() public constant returns (uint){
+        return totalSupply;
+    }
+
+    function balanceOf(address tokenOwner) public constant returns(uint balance)  {
+        return coinBalance[tokenOwner];
+    }
+
+    function allowance (address tokenOwner,address spender) public constant returns(uint remaining)  {
+        return allowed[tokenOwner][spender];
+    }
+        
+    function approve(address spender, uint tokens) public  returns (bool success){
+        uint haveToken = balanceOf(msg.sender);
+        require (haveToken>= tokens && tokens>0);
+        allowed[msg.sender][spender] = tokens;//address(msg.sender) -> [address(spender)->token(amount of acess)] 
+        emit  Approval(msg.sender, spender, tokens);
+        return true;
+    }
+    
+    function transfer(address to, uint tokens) public  returns (bool success){// from any  acc to any addres
+        uint haveToken = balanceOf(msg.sender);
+        require (haveToken>= tokens && tokens>0);
+        coinBalance[msg.sender]= coinBalance[msg.sender].sub(tokens);
+        coinBalance[to]  = coinBalance[to].add(tokens);
+        emit  Transfer(msg.sender, to, tokens);
+        return true;      
+    }
+        
+    function transferFrom(address from, address to, uint tokens) public  returns (bool success){
+        uint allowedToken = allowance(from , msg.sender);
+        require (allowedToken>0 && allowedToken>=tokens);
+        coinBalance[from]=  coinBalance[from].sub(tokens) ;
+        allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);//here msg.sender is the person having acess for acc of person
+        coinBalance[to] = coinBalance[to].add(tokens);
+        emit Transfer(from, to, tokens);
+        return true; 
+    }          
+    
+   /*  function getTokenOwner() public constant returns(address contractOwner){
+        return owner;
+    } */
+
+     /* only owner acessed function */    
+    function  burnTokens( uint burnTokensAmt) public onlyOwner returns (bool res)  {
+        require (coinBalance[owner] >= burnTokensAmt);      
+        totalSupply = totalSupply.sub(burnTokensAmt);// dec totalSupply by burned tokens
+        coinBalance[owner]=coinBalance[owner].sub(burnTokensAmt);//coin deducted to the owner coinbalance   
+        // emit  Burn(msg.sender, burnTokensAmt);
+        return true; 
+    }
+    function  mintCoin(uint mintTokens) public onlyOwner returns(bool res){
+        require(mintTokens>0);
+        totalSupply = totalSupply.add(mintTokens);//inc totalSupply by burned tokens 
+        coinBalance[owner]=coinBalance[owner].add(mintTokens);//coin added to the owner coinbalance
+
+        return true;
+    }
+
+   
+     /* only owner acessed function ENDS*/    
+
+}
+
+ 
+
